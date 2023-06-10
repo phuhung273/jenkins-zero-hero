0e862532364a72f494f6b119a3ed0448f3de595f
phuhung273 <phuhung.tranpham@gmail.com>
2023-06-10 21:49:01 +0700

    first job

---
+++ scripts/hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FIRST_NAME=$1
+LAST_NAME=$2
+SHOW=$3
+
+if [ "$SHOW" = true ]; then
+    echo "Hello $FIRST_NAME $LAST_NAME"
+else
+    echo "Check the show option if you want to see the name"
+fi
